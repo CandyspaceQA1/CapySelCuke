71165c7edd5ddabb5daf3f6493b14f1754c0f246
freelance01 <freelance01@Superted.local>
2014-10-22 15:07:14 +0100

    a

---
+++ Capybara.rb
@@ -0,0 +1,16 @@
+#require 'rubyzip'
+#require 'zip/zip'
+
+#require 'rubygems'
+#require 'selenium-webdriver'
+#require 'Capybara'
+#require 'Capybara/cucumber'
+#require 'rspec'
+
+#Capybara.current_driver = :selenium
+
+ 
+#visit "http://en.wikipedia.org/wiki/Baltimore_Ravens"
+
+
+
